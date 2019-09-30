240944179144d19c3c792dc7d01d7ce0166bf2de
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-30 20:49:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   def initialize(student_hash)
     @name = student_hash[:name]
     @location = student_hash[:location]
-    @@all << student_hash
+    @@all << @name 
   end
 
   def self.create_from_collection(students_array)
