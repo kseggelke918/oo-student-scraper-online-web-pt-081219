d20b2b3353b3c131e5f537733fd5dadcceddec6f
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-30 20:50:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,7 @@ class Student
     @name = student_hash[:name]
     @location = student_hash[:location]
     @@all << @name 
+    binding.pry 
   end
 
   def self.create_from_collection(students_array)
